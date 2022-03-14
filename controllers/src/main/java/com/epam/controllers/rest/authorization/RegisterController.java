a2f3f9bca0bfb1b546a8464285463272e536adb8
vadzim_yarosh <Vadzim_Yarosh@epam.com>
2022-03-14 18:09:06 +0300

    commit

--- controllers/src/main/java/com/epam/controllers/rest/authorization/RegisterController.java
+++ controllers/src/main/java/com/epam/controllers/rest/authorization/RegisterController.java
@@ -1,19 +1,17 @@
 package com.epam.controllers.rest.authorization;
 
-import com.epam.dto.authorization.RegisterFormDto;
-import com.epam.dto.cards.ReplenishmentFormDto;
-import com.epam.models.Client;
-import com.epam.models.Role;
-import com.epam.roles.Roles;
 import com.epam.config.security.utils.EncoderGenerator;
+import com.epam.dto.authorization.RegisterFormDto;
+import com.epam.models.Client;
 import com.epam.services.conrollers.ClientService;
 import com.epam.services.conrollers.RegistrationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
